645171102b06ec017f35865b3940c14e5b65bc8e
ErikPradoDev <erikpcdev@gmail.com>
2024-09-20 15:23:42 -0300

    15º commit - Header translation | Tradução do header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/header.sass'
 import ColumnTools from './ColumnTools';
+import '../i18n/i18nflts';
+import { useTranslation } from 'react-i18next'
+
 
 export default function Header(){
+    const { t, i18n } = useTranslation();
     return(
         <header> 
             <ColumnTools />
             <div className='MarginAuto1000px container'>
                 <div>
-                <Link to='/'> <p className='pLogo'> <span className='colorOrange'>&lt;</span>Epc <span className='colorOrange'>/&gt;</span> <span className='backHover'> | Back to Home </span> </p> </Link>
+                <Link to='/'> <p className='pLogo'> <span className='colorOrange'>&lt;</span>Epc <span className='colorOrange'>/&gt;</span> <span className='backHover'> {t('header.txtHover')} </span> </p> </Link>
                 </div>
                 <div>
                     <nav>
@@ -22,7 +26,7 @@ export default function Header(){
                                 <Link to='/blog'> <span className='itemNav'> <span className='colorOrange mrgnLeft'>#</span>Blog </span>  </Link>
                             </li>
                             <li>
-                                <Link to='/projects'> <span className='itemNav'> <span className='colorOrange mrgnLeft'>#</span>Projects </span> </Link>        
+                                <Link to='/projects'> <span className='itemNav'> <span className='colorOrange mrgnLeft'>#</span> {t('header.Projects')} </span> </Link>        
                             </li>
                         </ul>
                     </nav>
