0937df4e23db8c62c4672b4cf0fa6766ecbb975b
ErikPradoDev <erikpcdev@gmail.com>
2024-09-23 02:15:12 -0300

    22º commit - Insertion of Framer Motion on project:windowsDesktop | Inserção do Framer Motion no projeto:windowsDesktop

--- src/pages/projects/windowsPage.jsx
+++ src/pages/projects/windowsPage.jsx
@@ -5,78 +5,130 @@ import '../../styles/components/images.css';
 import { Link } from 'react-router-dom';
 import '../../i18n/i18nflts'
 import { useTranslation } from 'react-i18next'
+import { motion } from 'framer-motion';
 
 
-export default function WindowsPage(){
+export default function WindowsPage() {
     const { t, i18n } = useTranslation();
-    return(
+    return (
         <>
-        <main>
-            <section id="welcome">
+            <main>
+                <section id="welcome">
 
-            <div className='circleDctBottom'>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
+                    <div className='circleDctBottom'>
+                        <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                        <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                        <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                        <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
+                        <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
 
-                </div>
-
-                <div className='circleDctTop'>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
-                    <p> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
-
-                </div>
-
-                <div className="MarginAuto1100px">
-                    <h3> {t('sectionProject.subtitle')} </h3>
-                    <h2> Windows Desktop | Erik Prado </h2>
-                    <div className='lineSection'></div>
-
-                    <div className="MarginAuto900px">
-                    <p className="txt"> {t('pageProjectWindows.pResume')} </p>
-
-                    <div className="techLine marginTop10px">
-                         <p> #SASS </p>
-                         <p> #JavaScript </p>
-                         <p> #Git e GitHub </p>
-                         <p> #Vite </p>
-                         <p> #React </p>
-                         <p> #React Router </p>
                     </div>
 
-                    <div className="linkSizeContainer">
-                        <p> <Icon.Github className="icon" /> | {t('pageProjectWindows.LinkGitHub')} <Link to='https://github.com/ErikPradoDev/Portfolio' target="_blank" className="href" >https://github.com/ErikPradoDev/Portfolio</Link> </p>
-                        <p> <Icon.Linkedin className="icon" /> | {t('pageProjectWindows.LinkLinkedIn')} <Link to='https://www.linkedin.com/in/erikpradodev/'target="_blank" className="href">https://www.linkedin.com/in/erikpradodev/</Link> </p>
+                    <div className='circleDctTop'>
+                        <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                        <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                        <p> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                        <p> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
+                        <p> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
+
                     </div>
-                    </div>
-                </div>
-            </section>
 
-            <section id="articleWindows" className="MarginAuto900px">
+                    <motion.div className="MarginAuto1100px"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ ease: "easeOut", duration: 1.5 }}
+                        viewport={{ once: true }}
+                    >
+                        <h3> {t('sectionProject.subtitle')} </h3>
+                        <h2> Windows Desktop | Erik Prado </h2>
+                        <div className='lineSection'></div>
 
-                <p className="txt"> {t('pageProjectWindows.p1')} </p>
-                <p className="txt"> {t('pageProjectWindows.p2')} </p>
-                <p className="txt"> {t('pageProjectWindows.p3')} </p>
-                <p className="txt"> {t('pageProjectWindows.p4')}</p>
+                        <div className="MarginAuto900px">
+                            <p className="txt"> {t('pageProjectWindows.pResume')} </p>
 
-                <p className="imgPSize txt">   <span className="cLight"> {t('pageProjectWindows.p5')} </span> </p>
-                <div className="photoProjectContainer backgroundLockScreen"></div>
-                <p className="imgPTxt txt"> {t('pageProjectWindows.p6')} </p>
-                
-                <p className="imgPSize txt">   <span className="cLight"> {t('pageProjectWindows.p7')} </span> </p>
-                <div className="photoProjectContainer backgroundDesktop"></div>
-                <p className="imgPTxt txt"> {t('pageProjectWindows.p8')} </p>
-                
-                <p className="imgPSize txt">   <span className="cLight"> {t('pageProjectWindows.p9')} </span> </p>
-                <div className="photoProjectContainer backgroundErroPage"></div>
-                <p className="imgPTxt txt"> {t('pageProjectWindows.p10')} <br /> {t('pageProjectWindows.p11')} </p>
-            </section>
-        </main>
+                            <div className="techLine marginTop10px">
+                                <p> #SASS </p>
+                                <p> #JavaScript </p>
+                                <p> #Git e GitHub </p>
+                                <p> #Vite </p>
+                                <p> #React </p>
+                                <p> #React Router </p>
+                            </div>
+
+                            <div className="linkSizeContainer">
+                                <p> <Icon.Github className="icon" /> | {t('pageProjectWindows.LinkGitHub')} <Link to='https://github.com/ErikPradoDev/Portfolio' target="_blank" className="href" >https://github.com/ErikPradoDev/Portfolio</Link> </p>
+                                <p> <Icon.Linkedin className="icon" /> | {t('pageProjectWindows.LinkLinkedIn')} <Link to='https://www.linkedin.com/in/erikpradodev/' target="_blank" className="href">https://www.linkedin.com/in/erikpradodev/</Link> </p>
+                            </div>
+                        </div>
+                    </motion.div>
+                </section>
+
+                <section id="articleWindows" className="MarginAuto900px">
+
+                    <motion.section id="text"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ ease: "easeOut", duration: 1.5 }}
+                        viewport={{ once: true }}
+                    >
+                        <p className="txt"> {t('pageProjectWindows.p1')} </p>
+                        <p className="txt"> {t('pageProjectWindows.p2')} </p>
+                        <p className="txt"> {t('pageProjectWindows.p3')} </p>
+                    </motion.section>
+
+
+                    <motion.section id="firstScreen"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ ease: "easeOut", duration: 1.5 }}
+                        viewport={{ once: true }}
+                    >
+                        <h3>{t('pageProjectWindows.pFirstScren')}</h3>
+                        <h2> {t('pageProjectWindows.pLockScreen')} </h2>
+                        <div className='lineSection'></div>
+
+                        <div className="MarginAuto900px">
+                            <div className="photoProjectContainer backgroundLockScreen"></div>
+                            <p className=" txt"> {t('pageProjectWindows.p4')} </p>
+                            <p className=" txt"> {t('pageProjectWindows.p5')} </p>
+                        </div>
+                    </motion.section>
+
+
+                    <motion.section id="seccondScreen"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ ease: "easeOut", duration: 1.5 }}
+                        viewport={{ once: true }}
+                    >
+                        <h3>{t('pageProjectWindows.pSeccondScren')}</h3>
+                        <h2> {t('pageProjectWindows.pDesktopScreen')} </h2>
+                        <div className='lineSection'></div>
+
+                        <div className="MarginAuto900px">
+                            <div className="photoProjectContainer backgroundDesktop"></div>
+                            <p className=" txt"> {t('pageProjectWindows.p6')} </p>
+                        </div>
+                    </motion.section>
+
+
+                    <motion.section id="thirdScreen"
+                        initial={{ opacity: 0, x: -100 }}
+                        whileInView={{ opacity: 1, x: 0 }}
+                        transition={{ ease: "easeOut", duration: 1.5 }}
+                        viewport={{ once: true }}
+                    >
+                        <h3>{t('pageProjectWindows.pThirdScren')}</h3>
+                        <h2> {t('pageProjectWindows.pErrorPage')} </h2>
+                        <div className='lineSection'></div>
+
+                        <div className="MarginAuto900px">
+                            <div className="photoProjectContainer backgroundErroPage"></div>
+                            <p className="txt"> {t('pageProjectWindows.p7')} <br /> {t('pageProjectWindows.p8')} </p>
+                        </div>
+                    </motion.section>
+                </section>
+            </main>
 
         </>
     )
