d9b798629a5cd35ccd534f3c4a6a2cdb2278ce76
ErikPradoDev <erikpcdev@gmail.com>
2024-09-23 02:26:37 -0300

    23º commit - Insertion of Framer Motion on components:footer | Inserção do Framer Motion no components:footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,70 +4,76 @@ import '../styles/components/footer.sass'
 import '../i18n/i18nflts'
 import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 
 
-export default function Footer(){
+export default function Footer() {
     const { t, i18n } = useTranslation();
 
-    return(
+    return (
         <>
             <section id='contact'>
-            <div className='circleDctBottom'>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
+                <div className='circleDctBottom'>
+                    <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                    <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                    <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                    <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
+                    <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
 
                 </div>
 
                 <div className='circleDctTop'>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/>  </p>
-                    <p> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
-                    <p> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize circleNone'/> <Icon.CircleFill className='circleDctSize'/> <Icon.CircleFill className='circleDctSize'/> </p>
+                    <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                    <p> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                    <p> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' />  </p>
+                    <p> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
+                    <p> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize circleNone' /> <Icon.CircleFill className='circleDctSize' /> <Icon.CircleFill className='circleDctSize' /> </p>
 
                 </div>
 
-                <div className="MarginAuto1100px flex">
-                        <h3> {t('sectionContact.subtitle')} </h3>
-                        <h2> {t('sectionContact.title')} </h2>
-                            <div className='lineSection'></div>
+                <motion.div className="MarginAuto1100px flex"
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ ease: "easeOut", duration: 1.5 }}
+                    viewport={{ once: true }}
+                >
+                    <h3> {t('sectionContact.subtitle')} </h3>
+                    <h2> {t('sectionContact.title')} </h2>
+                    <div className='lineSection'></div>
 
-                <div className='formSize MarginAuto900px'>
-                    <p className='txt'> {t('sectionContact.p1')} <Link to='mailto:erikpcdev@gmail.com'> <span className='colorOrange'>erikpcdev@gmail.com</span></Link>. </p>
+                    <div className='formSize MarginAuto900px'>
+                        <p className='txt'> {t('sectionContact.p1')} <Link to='mailto:erikpcdev@gmail.com'> <span className='colorOrange'>erikpcdev@gmail.com</span></Link>. </p>
 
-                    <form>
+                        <form>
                             <label> {t('sectionContact.p2')} </label>
                             <input className='inputTxt' type="text" name="" id="" />
 
                             <label> {t('sectionContact.p3')} </label>
                             <input className='inputTxt' type="email" name="" id="" />
 
-                        
+                            <label> {t('sectionContact.p4')} </label>
+                            <textarea>
+                            </textarea>
 
+                            <div className='lineBtn'>
+                                <button className="btnFill" type="submit"> <Icon.SendCheckFill className="iconBtn" /> Enviar </button>
+                            </div>
+                        </form>
 
-
-                        <label> {t('sectionContact.p4')} </label>
-                        <textarea>
-
-                        </textarea>
-
-                        <div className='lineBtn'>
-                            <button className="btnFill" type="submit"> <Icon.SendCheckFill className="iconBtn" /> Enviar </button>
-                        </div>
-                    </form>
-
-                </div>
-                </div>
+                    </div>
+                </motion.div>
             </section>
 
-        <footer className="flex">
-            <p> Made with <Icon.HeartFill className="iconHeart"/> by <span>Erik Prado</span>.</p>
-        </footer>       
-        
+            <footer className="flex">
+                <motion.p
+                    initial={{ opacity: 0, x: -100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    transition={{ ease: "easeOut", duration: 1.5 }}
+                    viewport={{ once: true }}
+                > {t('footer.madeWith')} <Icon.HeartFill className="iconHeart" /> {t('footer.by')} <span>Erik Prado</span>.</motion.p>
+            </footer>
+
         </>
 
     )
